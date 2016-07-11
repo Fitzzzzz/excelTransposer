1d3f3655cc73d2930c7b243f7c689854071a80dd
Emmanuel HAMM <Emmanuel.hamm@enerdata.net>
2016-07-11 17:32:15 +0200

    Exceptions kind of handled

--- src/excelTransposer/Main.java
+++ src/excelTransposer/Main.java
@@ -2,6 +2,7 @@ package excelTransposer;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -10,7 +11,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
 		String inputName = ""; // TODO : REWORK because ugly af
 		int sheetNb = 0;
@@ -26,55 +27,69 @@ public class Main {
 			inputName = args[0];
 			sheetNb = Integer.parseInt(args[1]);
 			break;
+		default:
+			
+			break;	
 		}
 		
 		String[] parts = inputName.split("\\.");
-		String outputName = parts[0] + "NewTransposed.xlsx";
+		String outputName = parts[0] + "Transposed.xlsx";
 		
-		FileInputStream fis = new FileInputStream(new File(inputName));
-		XSSFWorkbook iWorkbook = new XSSFWorkbook(fis);
-	    XSSFSheet iSheet = iWorkbook.getSheetAt(sheetNb);
-	    
-	    XSSFWorkbook oWorkbook = new XSSFWorkbook();
-	    XSSFSheet oSheet = oWorkbook.createSheet(iSheet.getSheetName());
-	    
-	    
-	    SheetCouple duo = new SheetCouple(iSheet, oSheet, linesToCopy);
-	    
-	    duo.getInputFile().setSerieNb(serieNb);
-	    
-	    duo.copy(0, 0, linesToCopy);
-	    
-	    duo.writeHeader(linesToCopy);
-	    
-	    duo.writeBody();
-	    
-	    duo.insertComments();
-	    
-	    XSSFSheet iSuppSheet;
-	    
-	    if ((iSuppSheet = iWorkbook.getSheet("suppression")) != null) {
-	    	
-	    	XSSFSheet oSuppSheet = oWorkbook.createSheet("suppression");
-	    	SheetCouple duoSupp = new SheetCouple(iSuppSheet, oSuppSheet, linesToCopy);
-	    	
-	    	duoSupp.getInputFile().setSerieNb(serieNb);
+		try {
+			FileInputStream fis = new FileInputStream(new File(inputName));
+			XSSFWorkbook iWorkbook = new XSSFWorkbook(fis);
+		    XSSFSheet iSheet = iWorkbook.getSheetAt(sheetNb);
+		    XSSFWorkbook oWorkbook = new XSSFWorkbook();
+		    XSSFSheet oSheet = oWorkbook.createSheet(iSheet.getSheetName());
 		    
-		    duoSupp.copy(0, 0, linesToCopy);
 		    
-		    duoSupp.writeHeader(linesToCopy);
+		    SheetCouple duo = new SheetCouple(iSheet, oSheet, linesToCopy);
 		    
-		    duoSupp.writeBody();
+		    duo.getInputFile().setSerieNb(serieNb);
 		    
-		    duoSupp.insertComments();
+		    duo.copy(0, 0, linesToCopy);
 		    
-	    }
-	    
-	    
-	    FileOutputStream out = new FileOutputStream(new File(outputName));
-		oWorkbook.write(out);
-		out.close();
-	    
+		    duo.writeHeader(linesToCopy);
+		    
+		    duo.writeBody();
+		    
+		    duo.insertComments();
+		    
+		    XSSFSheet iSuppSheet;
+		    
+		    if ((iSuppSheet = iWorkbook.getSheet("suppression")) != null) {
+		    	
+		    	XSSFSheet oSuppSheet = oWorkbook.createSheet("suppression");
+		    	SheetCouple duoSupp = new SheetCouple(iSuppSheet, oSuppSheet, linesToCopy);
+		    	
+		    	duoSupp.getInputFile().setSerieNb(serieNb);
+			    
+			    duoSupp.copy(0, 0, linesToCopy);
+			    
+			    duoSupp.writeHeader(linesToCopy);
+			    
+			    duoSupp.writeBody();
+			    
+			    duoSupp.insertComments();
+			    
+		    }
+		    
+		    
+		    FileOutputStream out = new FileOutputStream(new File(outputName));
+			oWorkbook.write(out);
+			out.close();
+			iWorkbook.close();
+			oWorkbook.close();
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File " + inputName + " not found.");
+			System.out.println("Cancelling transposition of this file.");
+		} catch (IOException e) {
+			System.out.println("Unknown exception raised for file " + inputName + ".");
+			System.out.println("Cancelling transposition of this file.");
+			System.out.println("Stack Trace following : ");
+			e.printStackTrace();
+		} 
 		
 
 	}
